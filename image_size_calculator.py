4f970ec19f6eb5f3be11e1897d26ff251ebb32a4
gnarfbert <alanlin317@gmail.com>
2025-06-27 19:14:24 -0400

    Implemented the calculations for the image size.

--- image_size_calculator.py
+++ image_size_calculator.py
@@ -1,5 +1,18 @@
+import os as o
 import numpy as np
-import cv2 
+import cv2 as cv
 
 
+global image_map
+image_map = {}
 
+
+def get_image_size(image_path: str) -> int:
+    image = cv.imread(image_path)
+    height, width, color = image.shape
+    
+
+    return height
+
+
+print(get_image_size("test-images/image-1.png"))
--- test_calc_size.py
+++ test_calc_size.py
@@ -1,4 +1,8 @@
 import importlib
 import pytest
 
+import image_size_calculator as img_calculator
 
+def test_get_image_size():
+    image_path = '/test-images/image-1.png'
+    assert img_calculator.get_image_size(image_path) = 
