06273f648036bb86b7327572c1c1b08db2061bd4
gnarfbert <alanlin317@gmail.com>
2025-07-26 20:08:34 -0400

    Created the function to identify the song's genre Used BeautifulSoup to scrap the song's genre from the inputted URL link

--- code_dj.py
+++ code_dj.py
@@ -1,23 +1,15 @@
 import requests as rq
 from bs4 import BeautifulSoup 
+from heapq import heappop, heappush
 
 
 
 global common_phrases
 common_phrases = set()
 
+LYRICS_CONTAINER = 'Lyrics__Container-sc-3d1d18a3-1 bjajog'
+SONG_TAGS_CONTAINER = 'SongTags__Container-sc-b55131f0-1 SEhjw'
 
-
-# response = rq.get('https://genius.com/Fugees-killing-me-softly-with-his-song-lyrics')
-
-# soup = BeautifulSoup(response.text, 'html.parser')
-
-# content_div = soup.find('div', class_='Lyrics__Container-sc-3d1d18a3-1 bjajog')
-
-# split = content_div.text.split('Read More')
-
-# with open('content.txt', 'w') as file:
-#     file.write(split[1])
 
 def setup() -> None:
     global common_phrases
@@ -29,29 +21,81 @@ def setup() -> None:
     
         
 
-def scrap_lyrics(url: str) -> None:
+def scrap_lyrics(url: str) -> str:
     if 'genius.com' in url and 'lyrics' in url:    
         response = rq.get(url)
         soup = BeautifulSoup(response.text, 'html.parser')
-        lyric_text = soup.find('div', class_='Lyrics__Container-sc-3d1d18a3-1 bjajog').text
+        lyric_text = soup.find('div', class_= LYRICS_CONTAINER).text
         lyric_text = lyric_text.split('Read More')[1]
-        
+
         if lyric_text:
             with open('lyrics.txt', 'w', encoding='utf-8') as file:
                 file.write(lyric_text)
+                return lyric_text
+
     else:
         print('oopsies! you have inputted an unsupported link. try to upload a link from Genius.')
 
 
 
-def record_word_count() -> None:
+def record_word_count(url: str) -> list:
 
+    global common_phrases
+    word_frequency = {}
+
+    lyrics = scrap_lyrics(url)
+    lyrics_list = lyrics.split(' ')
+
+    for word in lyrics_list:
+        if word.lower() not in common_phrases:
+            word_frequency[word] = 1 + word_frequency.get(word, 0)   
+
+    max_heap = []
+
+    for key in word_frequency.keys():
+        heappush(max_heap, (-1 * word_frequency[key], key))
     
+    top_five_frequent_words = []
 
+    while len(top_five_frequent_words) < 5:
+        top_five_frequent_words.append(heappop(max_heap)[1])
+
+    return top_five_frequent_words
+
+
+def get_song_genre(url:str) -> str:
+
+    if 'genius' in url and 'lyrics' in url:
+        response = rq.get(url)
+        soup = BeautifulSoup(response.text, 'html.parser')
+        genre = soup.find('div', class_= SONG_TAGS_CONTAINER).text
+        
+        return genre
+    
+    else:
+        print('oopsies! you have inputted an unsupported link. try to upload a link from Genius.')
+
+
+
+def find_similar_song_genre() -> list:
+    with open('spotify_songs.csv', 'r') as file:
+        file.readline()
+
+
+        return None
+
+
+
+def compare_songs_by_lyrics(url:str) -> list:
     return None
+
+
 
 
 setup()
 
-scrap_lyrics('https://genius.com/Kendrick-lamar-tv-off-lyrics')
+print(record_word_count('https://genius.com/Clipse-so-be-it-lyrics'))
+
+print(get_song_genre('https://genius.com/Clipse-so-be-it-lyrics'))
+
 
