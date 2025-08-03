b6449eb0b37325b8ab04094d9316a8577ab79940
gnarfbert <alanlin317@gmail.com>
2025-08-03 19:05:20 -0400

    Tweaked the lyric parser Added a try,except block to the lyric parser Refactored the function to get similar songs

--- code_dj.py
+++ code_dj.py
@@ -1,46 +1,48 @@
 import requests as rq
 import spacy
+import re 
 from bs4 import BeautifulSoup 
-from spacy.lang.en import stop_words
 
 
-nlp = spacy.load("en_core_web_md")
+nlp = spacy.load('en_core_web_md')
 
 
 
 LYRICS_CONTAINER = 'Lyrics__Container-sc-3d1d18a3-1 bjajog'
+LYRICS_HEADER = 'LyricsHeader__Container-sc-5af38d6b-1 ksyvqp'
 SONG_TAGS_CONTAINER = 'SongTags__Container-sc-b55131f0-1 SEhjw'
 
 
     
     
 def get_song_lyrics(url: str) -> str:
-    if 'genius.com' in url and 'lyrics' in url:    
+    try:   
         response = rq.get(url)
         soup = BeautifulSoup(response.text, 'html.parser')
 
-        lyric_container = soup.find('div', class_= LYRICS_CONTAINER).get_text(separator=' ')
+        lyric_container = soup.find('div', class_= LYRICS_CONTAINER).find('div', class_= LYRICS_HEADER).decompose()
+        lyric_container = soup.find('div', class_= LYRICS_CONTAINER).get_text(separator= ' ')
 
-        lyrics = lyric_container.split('Read More')[1]
+        lyrics = re.split(r'\xa0', lyric_container)[0]
+        print(len(lyrics))
 
         if lyrics:
-            with open('lyrics.txt', 'w', encoding='utf-8') as file:
-                file.write(lyrics)
+            return lyrics
 
-                return lyrics
-
-    else:
-        print('oopsies! you have inputted an unsupported link. try to upload a link from Genius.')
+    except AttributeError:
+        pass
 
 
 
 def compare_lyrics_similarity(song1: str, song2:str) -> int:
     lyrics1 = nlp(song1)
     lyrics2 = nlp(song2)
-    filtered_lyrics1 = nlp(' '.join([str(word) for word in lyrics1 if not word.is_stop]))
-    filtered_lyrics2 = nlp(' '.join([str(word) for word in lyrics2 if not word.is_stop]))
+    filtered_lyrics1 = nlp(' '.join([str(word) for word in lyrics1 if word.pos_ in ['NOUN', 'PROPN']]))
+    filtered_lyrics2 = nlp(' '.join([str(word) for word in lyrics2 if word.pos_ in ['NOUN', 'PROPN']]))
 
-
+    print(filtered_lyrics1)
+    print(filtered_lyrics2)
+    print(filtered_lyrics1.similarity(filtered_lyrics2))
     return filtered_lyrics1.similarity(filtered_lyrics2)
 
 
@@ -58,7 +60,7 @@ def get_song_genre(url:str) -> str:
 
 
 
-def get_similar_songs(target_genre:str,target_artist:str) -> list:
+def get_similar_songs(target_genre:str) -> list:
     similar_songs = []
     with open('spotify_songs.csv', 'r', encoding= 'utf-8') as file:
         file.readline()
@@ -71,14 +73,18 @@ def get_similar_songs(target_genre:str,target_artist:str) -> list:
             song_information = line.split(',')
             
             song_genre = song_information[9]
-            song_artist = song_information[2]
 
-            if (song_genre.lower() == target_genre.lower() and 
-                song_artist.lower() == target_artist.lower()):
+            if song_genre.lower() == target_genre.lower():
                 
                 song_name = song_information[1]
+                song_artist = song_information[2]
+
+                song_artist = song_artist.replace(' ', '-')
+                song_name = re.sub('\s*\(feat\..*?\)\s*', '',song_name).replace(' ', '-')
+                genius_url = f'https://www.genius.com/{song_artist}-{song_name}-lyrics'
+
                 
-                similar_songs.append((song_name, song_artist))
+                similar_songs.append(genius_url)
 
         return similar_songs
 
@@ -88,32 +94,12 @@ def compare_songs_by_lyrics(url:str) -> list:
 
     recommended_songs = []
 
-    frequent_lyric_count = compare_lyrics_similarity(url)
+    user_song_lyrics = get_song_lyrics(url)
     target_genre = get_song_genre(url)
-    similar_songs = get_similar_songs(target_genre, 'drake')
+    similar_songs = get_similar_songs(target_genre)
 
 
 
-    for song in similar_songs:
-
-        song_name, song_artist = song
-
-        song_artist = song_artist.replace(' ', '-')
-
-        song_name = re.sub('\s*\(feat\..*?\)\s*', '',song_name).replace(' ', '-')
-
-        query = f'https://www.genius.com/{song_artist}-{song_name}-lyrics'
-        
-
-        try:
-            similar_song_lyrics = sorted(get_song_lyrics(query))
-            frequent_lyric_count = sorted(frequent_lyric_count)
-
-
-        except IndexError:
-            continue
-        except AttributeError:
-            continue
 
 
 
@@ -136,8 +122,8 @@ def compare_songs_by_lyrics(url:str) -> list:
 
 # get_similar_songs('rap','drake')
 
+print(get_similar_songs('rap'))
 
-compare_lyrics_similarity('hello he wants jimmy dinner', 'he went to china to get dumplings')
 
 
 
