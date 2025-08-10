7e0a5a3276971845e0755db86df5a2ac75cdf3c1
gnarfbert <alanlin317@gmail.com>
2025-08-10 16:32:53 -0400

    Added documentation Finished the recommendation function.

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# image-by-size
+# Code-DJ
+Code DJ is a program that recommends five new songs based on one inputted song.
+
+## Installation 
+Create a Python virtual environment, make sure to check the box next to requirement.txt.
+
+## Process
+- I used the song's energy, tempo, genre, and loudness range to filter songs in the dataset.
+- With these similar songs, I used web-scraping techniques to record the lyrics of five randomly selected songs.
+- Lastly, I used Natural Language Processing with the spaCy library to compare the similarity score between the songs.
+
+
+
--- code_dj.py
+++ code_dj.py
@@ -1,58 +1,113 @@
 import requests as rq
 import spacy
 import re 
+import concurrent.futures
+import random
 from bs4 import BeautifulSoup 
 
 
 nlp = spacy.load('en_core_web_md')
 
 
-
-LYRICS_CONTAINER = 'Lyrics__Container-sc-3d1d18a3-1 bjajog'
+# DO NOT EDIT: These constant values help the scraping function identify the target HTML elements
+LYRICS_CONTAINER = 'Lyrics__Container-sc-39b434ea-1 gHGicG'
 LYRICS_HEADER = 'LyricsHeader__Container-sc-5af38d6b-1 ksyvqp'
 SONG_TAGS_CONTAINER = 'SongTags__Container-sc-b55131f0-1 SEhjw'
+MAX_THREADS = 6
 
 
-    
-    
+def play_session() -> None:
+    print('Welcome to Python DJ!')
+    user_song = input('Please enter a Genius url to a song you like!\n')
+    recommended = compare_songs_by_lyrics(user_song)
+    print('Searching for similar songs...')
+
+    for song in recommended:
+        song_name, artist = song
+        print(f'Check out {song_name} by {artist}!')
+
+
 def get_song_lyrics(url: str) -> str:
+    """
+    This function scrapes the lyrics from the inputted URL.
+
+    Parameter:
+        url: A string that represents the genius.com URL of the song
+
+    Return:
+        A string that represents the lyrics of the song 
+    """
+
     try:   
         response = rq.get(url)
         soup = BeautifulSoup(response.text, 'html.parser')
 
-        lyric_container = soup.find('div', class_= LYRICS_CONTAINER).find('div', class_= LYRICS_HEADER).decompose()
-        lyric_container = soup.find('div', class_= LYRICS_CONTAINER).get_text(separator= ' ')
+        lyric_container = soup.select_one('div[data-lyrics-container="true"]').select_one('div[data-exclude-from-selection="true"]').decompose()
+        lyric_container = soup.select_one('div[data-lyrics-container="true"]').get_text(separator= ' ')
 
         lyrics = re.split(r'\xa0', lyric_container)[0]
-        print(len(lyrics))
-
         if lyrics:
             return lyrics
 
     except AttributeError:
-        pass
+        return 'no lyrics'
 
+def parse_song_links(song_info: tuple) -> tuple:
+    """
+    This is a wrapper function that returns a tuple of the song lyrics,
+    song name, and the artist's name
 
+    Parameter:
+        song_info: A tuple containing the URL of the song, the song name, and
+        the artist's name
+    
+    Return:
+        A tuple that contains the song lyrics, song name, and artist name
+    """
+
+    song_url, song_name, song_artist = song_info
+
+    song_lyrics = get_song_lyrics(song_url)
+
+    return (song_lyrics, song_name, song_artist)
 
 def compare_lyrics_similarity(song1: str, song2:str) -> int:
+    """
+    This function returns the similarity score between two songs by comparing
+    the similarity between the nouns in each song
+
+    Parameters:
+        song1: A string containing the lyrics of the first song
+        song2: A string containing the lyrics of the second song
+    
+    Return:
+        A integer value between 0.0 and 1.0 that represents the similarity 
+        of the two songs.
+    """
+
     lyrics1 = nlp(song1)
     lyrics2 = nlp(song2)
     filtered_lyrics1 = nlp(' '.join([str(word) for word in lyrics1 if word.pos_ in ['NOUN', 'PROPN']]))
     filtered_lyrics2 = nlp(' '.join([str(word) for word in lyrics2 if word.pos_ in ['NOUN', 'PROPN']]))
 
-    print(filtered_lyrics1)
-    print(filtered_lyrics2)
-    print(filtered_lyrics1.similarity(filtered_lyrics2))
+
     return filtered_lyrics1.similarity(filtered_lyrics2)
 
 
 def get_song_genre(url:str) -> str:
+    """
+    This function identifies the genre of the inputted song
 
+    Parameter:
+        url: A string that represents the url of the song
+    
+    Returns:
+        A string that represents the song's genre 
+    """
     if 'genius' in url and 'lyrics' in url:
         response = rq.get(url)
         soup = BeautifulSoup(response.text, 'html.parser')
         genre = soup.find('div', class_= SONG_TAGS_CONTAINER).contents[0].text
-        
         return genre
     
     else:
@@ -60,9 +115,22 @@ def get_song_genre(url:str) -> str:
 
 
 
-def get_similar_songs(target_genre:str) -> list:
+def get_similar_songs(target_genre:str) -> list[tuple]:
+    """
+    This function finds the similar songs based on the inputted genre
+
+    Parameter:
+        target_genre: A string that represents the genre that the user
+        wants similar songs of
+    
+    Return:
+        A list of tuples containing the song URL, song name, and artist
+        name
+    """
+
     similar_songs = []
-    with open('spotify_songs.csv', 'r', encoding= 'utf-8') as file:
+    cache = set()
+    with open('spotify_songs_final_filtered_6.csv', 'r', encoding='utf-8') as file:
         file.readline()
 
         while True:
@@ -73,24 +141,36 @@ def get_similar_songs(target_genre:str) -> list:
             song_information = line.split(',')
             
             song_genre = song_information[9]
-
-            if song_genre.lower() == target_genre.lower():
+            song_name = song_information[1]
+        
+            if (song_genre.lower() == target_genre.lower() and
+                not re.search(r'\(.*?\)', song_name) and
+                song_name not in cache):
                 
-                song_name = song_information[1]
+                cache.add(song_name)
                 song_artist = song_information[2]
-
+                song_name = song_name.replace(' ', '-')
                 song_artist = song_artist.replace(' ', '-')
-                song_name = re.sub('\s*\(feat\..*?\)\s*', '',song_name).replace(' ', '-')
                 genius_url = f'https://www.genius.com/{song_artist}-{song_name}-lyrics'
 
                 
-                similar_songs.append(genius_url)
+                similar_songs.append((genius_url, song_name, song_artist))
 
         return similar_songs
 
 
 
-def compare_songs_by_lyrics(url:str) -> list:
+def compare_songs_by_lyrics(url:str) -> list[tuple]:
+    """
+    This function returns five recommended songs based on the user's 
+    inputted song
+
+    Parameter:
+        url: A string that represents the url of the song
+    
+    Returns:
+        A list of tuples that contains the song name and the artist's name
+    """
 
     recommended_songs = []
 
@@ -99,31 +179,26 @@ def compare_songs_by_lyrics(url:str) -> list:
     similar_songs = get_similar_songs(target_genre)
 
 
+    while len(recommended_songs) < 5:
+
+        result = []
+        for i in range(6):
+            random_index = random.randrange(0, len(similar_songs))
+            result.append(similar_songs.pop(random_index))
+
+        with concurrent.futures.ThreadPoolExecutor(max_workers=MAX_THREADS) as executor:
+            result = executor.map(parse_song_links,result)
+        
+        for song in result:
+            song_lyrics, song_name, song_artist = song
+
+            if compare_lyrics_similarity(user_song_lyrics, song_lyrics) > 0.8:
+                recommended_songs.append((song_name, song_artist))
+    
+    return recommended_songs
 
 
-
-
-
-
-
-    print(recommended_songs)
-
-
-
-
-
-# print(record_word_count('https://genius.com/Clipse-so-be-it-lyrics'))
-
-# print(get_song_genre('https://genius.com/Clipse-so-be-it-lyrics'))
-
-# get_song_lyrics('https://genius.com/Clipse-so-be-it-lyrics')
-
-# compare_songs_by_lyrics('https://genius.com/Clipse-so-be-it-lyrics')
-
-# get_similar_songs('rap','drake')
-
-print(get_similar_songs('rap'))
-
+play_session()
 
 
 
